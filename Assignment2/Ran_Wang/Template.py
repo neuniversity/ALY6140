6a236d1e543c4d054b45e73d2cf442f2e6c48bdf
ran <ran@randeMacBook-Pro.local>
2018-11-13 15:10:04 -0800

    update

---
+++ Assignment2/Ran_Wang/Template.py
@@ -0,0 +1,34 @@
+"""
+<Project Title>
+
+
+Copyright (c) 2017
+Licensed
+Written by <>
+"""
+
+
+
+def function1(var1,var2,var2):
+    """
+
+    :param var1:
+    :param var2:
+    :return:
+    """
+
+    return
+
+def function2(var1,var2,var3):
+    """
+
+    :param var1:
+    :param var2:
+    :param var3:
+    :return:
+    """
+
+    return
+
+if __name__ == "__main__":
+    # Main functions to Run
